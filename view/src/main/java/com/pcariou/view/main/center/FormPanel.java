25c5d4422c43a7c9b1d0f513097098565a12cde6
Pierre Cariou <pierrecariou@outlook.fr>
2026-06-15 13:40:13 +0200

    V1.3.0 Community Release

--- view/src/main/java/com/pcariou/view/main/center/FormPanel.java
+++ view/src/main/java/com/pcariou/view/main/center/FormPanel.java
@@ -106,7 +106,7 @@ public class FormPanel extends JPanel implements Scrollable {
         JLabel inputLabel = new JLabel("Input file");
         inputLabel.setForeground(UIManager.getColor("Label.disabledForeground"));
 
-        JPanel inputColumn = new JPanel(new MigLayout("insets 0, fillx, gapy 2", "[grow,fill]", "[][]"));
+        JPanel inputColumn = new JPanel(new MigLayout("insets 0, fillx, gapy 2", "[grow]", "[][]"));
         inputColumn.setOpaque(false);
         inputColumn.add(inputField, "growx, wrap");
         inputColumn.add(createTemplateLink(), "alignx left, gapleft 0");
@@ -234,14 +234,6 @@ public class FormPanel extends JPanel implements Scrollable {
                     try { Desktop.getDesktop().open(new File(filenameOutput)); }
                     catch (Exception ex) { showLocalError("Could not open the file:\n" + ex.getMessage()); }
                 }
-            }
-            @Override public void mouseEntered(MouseEvent e) {
-                summaryFileName.putClientProperty(FlatClientProperties.STYLE,
-                        "foreground: $Component.accentColor; font: bold;");
-            }
-            @Override public void mouseExited(MouseEvent e) {
-                summaryFileName.putClientProperty(FlatClientProperties.STYLE,
-                        "foreground: $Component.accentColor;");
             }
         });
         Links.asLink(summaryFileName);
